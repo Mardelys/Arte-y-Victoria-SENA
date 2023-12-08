8cede8922152ad3a5733307450f6be8e11fed490
mardelys <mardelys1996@gmail.com>
2023-12-07 22:16:49 -0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Header from "@/Components/header/Header"
+
+export default function Home() {
+  return (
+    <main >
+       <main >
+      <Header />
+      <p>Mardelys</p>
+
+    </main>
+    </main>
+  )
+}
